4abbc6a403eac90d2e2e8d3aed03512fc253ee91
nirchoudhary <139118342+nirchoudhary@users.noreply.github.com>
2024-11-28 20:49:16 +0530

    square free number

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Integer Number : ");
+        int n = sc.nextInt();
+
+        int count = 0;
+        ArrayList<Integer> factors = new ArrayList<>();
+        System.out.print("Factors : 1 ");
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+                if (isSquareFree(i)) {
+                    factors.add(i);
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("\nTotal square free factors : " + count + "\nfactors are : " + factors);
+    }
+
+    static boolean isSquareFree(int factor) {
+        for (int i = 2; i * i <= factor; i++) {
+            if (factor % (i * i) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
